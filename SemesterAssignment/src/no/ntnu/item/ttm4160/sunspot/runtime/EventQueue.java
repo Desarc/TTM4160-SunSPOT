745c52f74cec9323ecc398ce447773f4cc85375c
Snorre Lothar von Gohren Edwin <snorre.edwin@gmail.com>
2012-10-05 14:16:24 +0200

    started the scheduler task, and registerd the listener, hopefully. Also started to modify evnetqueue to work in scheduler

--- SemesterAssignment/src/no/ntnu/item/ttm4160/sunspot/runtime/EventQueue.java
+++ SemesterAssignment/src/no/ntnu/item/ttm4160/sunspot/runtime/EventQueue.java
@@ -1,10 +1,25 @@
 package no.ntnu.item.ttm4160.sunspot.runtime;
 
 import com.sun.spot.util.Queue;
+
+import no.ntnu.item.ttm4160.sunspot.communication.Message;
 import no.ntnu.item.ttm4160.sunspot.utils.*;
 
 public class EventQueue {
 
 	private Queue EventQueue;
+	private Event event;
+	
+	public EventQueue() {
+		EventQueue = new Queue();
+	}
+	
+	public void saveMessage(Message msg) {
+		EventQueue.put(msg);
+	}
+	
+	public void getMessage() {
+		
+	}
 	
 }
